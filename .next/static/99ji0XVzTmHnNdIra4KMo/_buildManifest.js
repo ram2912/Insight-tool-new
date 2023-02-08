913d77eb0b491cd9178723e7070198590d98adb7
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-02-08 13:33:51 +0100

    Initial build

--- .next/static/99ji0XVzTmHnNdIra4KMo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f1d1e8757da0e9f4.css","static/chunks/pages/index-ed8dfd6ffbe6266a.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
